19cc0fa48ad093f2672b2e15e59087d4bba54c08
Olutomiwaxl <oloyedeolutomiwa@yahoo.com>
2019-07-20 11:42:44 -0500

    setting up heroku

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import pandas as pd 
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("index")
 
 if __name__ == '__main__':
     app.run()
